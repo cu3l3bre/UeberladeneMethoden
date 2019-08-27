441e405281f5c57414e407cc9c72a93e694c95aa
cu3l3bre <cu3l3bre@googlemail.com>
2019-08-27 12:06:12 +0200

    Projektdateien hinzufügen.

---
+++ UeberladeneMethoden/MethodenUeberladen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UeberladeneMethoden
+{
+    public static class MethodenUeberladen
+    {
+
+        public static void BubbleSort(double[] werte)
+        {
+            for (int i = 0; i < werte.Length - 1; i++)
+            {
+                for (int j = i + 1; j < werte.Length; j++)
+                {
+                    if (werte[i] > werte[j])
+                    {
+                        double tmp = werte[i];
+                        werte[i] = werte[j];
+                        werte[j] = tmp;
+                    }
+                }
+            }
+        }
+
+
+        public static void BubbleSort(double[] werte, bool orderDesc) // false = asecnding, true = descending 
+        {
+            for (int i = 0; i < werte.Length - 1; i++)
+            {
+                for (int j = i + 1; j < werte.Length; j++)
+                {               
+                    //Console.WriteLine("Aufsteigend...");
+                    if ((!orderDesc && werte[i] > werte[j]) || (orderDesc && werte[i] < werte[j]))
+                    {
+                        double tmp = werte[i];
+                        werte[i] = werte[j];
+                        werte[j] = tmp;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ UeberladeneMethoden/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UeberladeneMethoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] test = { 2.0, 8.0, 3.0, 19.0, 32.0, 12.0, 99.0, 55.0, 1.0 };
+            double[] test2 = { 2.0, 8.0, 3.0, 19.0, 32.0, 12.0, 99.0, 55.0, 1.0 };  // test;
+
+            //double[] test2 = test; // Verweis auf einen Verweis, so wüde der selbe Datensatz bearbeitet
+
+
+            Console.Write("          Ungeordnet: ");
+            foreach (double wert in test)
+            {
+                Console.Write(wert + " ");
+            }
+            Console.WriteLine();
+
+
+            MethodenUeberladen.BubbleSort(test, false); // Aufsteigend
+            MethodenUeberladen.BubbleSort(test2, true); // Absteigend
+
+
+            Console.Write("Aufsteigend geordnet: ");
+            foreach (double wert in test)
+            {
+                Console.Write(wert + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.Write(" Absteigend geordnet: ");
+            foreach (double wert in test2)
+            {
+                Console.Write(wert + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
